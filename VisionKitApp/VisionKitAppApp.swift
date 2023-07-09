8d175cd0d17926fe5e767f23ed399d735124b606
tokomac <tokomatokomac@gmail.com>
2023-07-09 15:05:40 +0900

    Initial Commit

---
+++ VisionKitApp/VisionKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionKitAppApp.swift
+//  VisionKitApp
+//
+//  Created by 木科誠 on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct VisionKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
